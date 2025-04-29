31a071aab3923d5fa81edd44d6ef999e1283ae0e
Nicholas Mirallegro <149200502+njmirallegro@users.noreply.github.com>
2025-04-29 19:25:28 -0400

    Update zsh_profile_setup.sh

--- shell_setup/zsh_profile_setup.sh
+++ shell_setup/zsh_profile_setup.sh
@@ -61,7 +61,7 @@ if ! grep -q ".p10k.zsh" ~/.zshrc; then
 fi
 
 # Download custom .p10k.zsh from GitHub
-curl -fsSL https://raw.githubusercontent.com/njmirallegro/setup/main/.p10k.zsh -o ~/.p10k.zsh
+curl -fsSL https://raw.githubusercontent.com/njmirallegro/workspace_setup/main/shell_setup/.p10k.zsh -o ~/.p10k.zsh
 
 # Set default shell to Zsh
 ZSH_PATH=""
